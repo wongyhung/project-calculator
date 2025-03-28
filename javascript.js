53ed872f362b0e7d04bd171ec256c9a01fd45bfe
Wong Yhung <wongyhung@gmail.com>
2025-03-28 22:24:55 +0800

    + - * / buttons no longer duplicate at the end of displayNum

--- javascript.js
+++ javascript.js
@@ -16,12 +16,6 @@ function divide(num1, num2) {
 
 let displayNum, inputNum1, operator, inputNum2;
 
-function operate(num1, op, num2) {
-    if (op === 1) {
-        add(num1, num2);
-    }
-}
-
 const acButton = document.querySelector("#AC");
 const plusMinusButton = document.querySelector("#plus-minus");
 const percentButton = document.querySelector("#percent");
@@ -41,10 +35,66 @@ function updateDisplay() {
     screen.textContent = displayNum;
 }
 
+function operate(num1, op, num2) {
+    if (op === 1) {
+        add(num1, num2);
+    } else if (op === 2) {
+        subtract(num1, num2);
+    } else if (op === 3) {
+        multiply(num1, num2);
+    } else if (op === 4) {
+        divide(num1, num2);
+    }
+}
+
 const numberButtons = document.querySelectorAll("[id^='num']");
 numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         displayNum += button.textContent;
         updateDisplay();
     });
+});
+
+addButton.addEventListener("click", () => {
+    if (["-", "x", "÷"].includes(displayNum.charAt(displayNum.length - 1))) {
+        displayNum = displayNum.slice(0, displayNum.length - 1);
+    }
+    if (displayNum.charAt(displayNum.length - 1) !== "+") {
+        operator = 1;
+        displayNum += addButton.textContent;
+        updateDisplay();
+    }
+});
+
+subtractButton.addEventListener("click", () => {
+    if (["+", "x", "÷"].includes(displayNum.charAt(displayNum.length - 1))) {
+        displayNum = displayNum.slice(0, displayNum.length - 1);
+    }
+    if (displayNum.charAt(displayNum.length - 1) !== "-") {
+        operator = 2;
+        displayNum += subtractButton.textContent;
+        updateDisplay();
+    }
+});
+
+multiplyButton.addEventListener("click", () => {
+    if (["-", "+", "÷"].includes(displayNum.charAt(displayNum.length - 1))) {
+        displayNum = displayNum.slice(0, displayNum.length - 1);
+    }
+    if (displayNum.charAt(displayNum.length - 1) !== "x") {
+        operator = 3;
+        displayNum += multiplyButton.textContent;
+        updateDisplay();
+    }
+});
+
+divideButton.addEventListener("click", () => {
+    if (["-", "x", "+"].includes(displayNum.charAt(displayNum.length - 1))) {
+        displayNum = displayNum.slice(0, displayNum.length - 1);
+    }
+    if (displayNum.charAt(displayNum.length - 1) !== "÷") {
+        operator = 4;
+        displayNum += divideButton.textContent;
+        updateDisplay();
+    }
 });
